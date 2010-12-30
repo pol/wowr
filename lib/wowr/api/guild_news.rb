7f0de3f9978e295610c7f0536b222b2e8d2490dd
Pol Llovet <pol.llovet@gmail.com>
2010-12-30 14:51:27 -0700

    Update guild_news_url parsing, get_guild_news

--- lib/wowr/api/guild_news.rb
+++ lib/wowr/api/guild_news.rb
@@ -1,12 +1,24 @@
 module Wowr
   module API
     module GuildNews
+      require 'cgi'
+      
       def self.included(base)
         base.class_eval do
           @@guild_news_url    = 'guild/%s/%s/news'.freeze
-
-          cattr_reader :guild_news_url
         end
+      end
+
+
+      def guild_news_url(options = {})
+        check_guild_news_options(options)
+
+        # Space to hyphen, everything else omitted
+        r = options[:realm].downcase.gsub(/[^\w ]/,'').gsub(/ /,'-')
+        # URL encoded
+        gn = CGI.escape(options[:guild_name].downcase)
+        
+        @@guild_news_url % [r,gn]
       end
       
       # Get the guild news.
@@ -14,28 +26,32 @@ module Wowr
       # Requires realm.
       # * name (String) Name of the guild to retrieve, defaults to that specified in constructor
       # * options (Hash) Optional hash of arguments identical to those used in the API constructor (realm, debug, cache etc.)
-      def get_guild_news(name = @guild_name, options = {})
-      #         if (name.is_a?(Hash))
-      #           options = name
-      #         else
-      #           options.merge!(:guild_name => name)
-      #         end
-      # 
-      #         options = merge_defaults(options)
-      # 
-              if options[:guild_name].nil? || options[:guild_name].blank?
-                raise Wowr::Exceptions::GuildNameNotSet.new
-              elsif options[:realm].nil? || options[:realm].empty?
-                raise Wowr::Exceptions::RealmNotSet.new
-              end
-      # 
-      #         xml = get_xml(@@guild_info_url, options)
-      # 
-        # unless (xml%'guildInfo').nil? || (xml%'guildInfo').children.empty?
-        #   return Wowr::Classes::FullGuild.new(xml)
-        # else
-        #   raise Wowr::Exceptions::GuildNotFound.new(options[:guild_name])
-        # end
+      def get_guild_news(options = {})
+        check_guild_news_options(options)
+        
+        url = guild_news_url(options)
+        news_list = get_html(url).search('ul#news-list')
+        
+        if news_list.nil?
+          raise Wowr::Exceptions::GuildNotFound.new(options[:guild_name])
+        else
+          return Wowr::Classes::GuildNews.new(news_list)
+        end
+      end
+      
+      private
+      
+      def check_guild_news_options(options)
+        options[:name]  ||= @guild_name
+        options[:realm] ||= @realm
+
+        if options[:guild_name].nil? || options[:guild_name].blank?
+          raise Wowr::Exceptions::GuildNameNotSet.new
+        end
+        
+        if options[:realm].nil? || options[:realm].empty?
+          raise Wowr::Exceptions::RealmNotSet.new
+        end        
       end
     end
   end
