10a6472e2710513b7a5ae2e1cd0eaef182711532
Pol Llovet <pol.llovet@gmail.com>
2010-12-30 14:53:59 -0700

    Add base GuildNews class

---
+++ lib/wowr/armory/guild_news.rb
@@ -0,0 +1,9 @@
+$:.unshift(File.dirname(__FILE__))
+
+module Wowr
+  module Armory
+    module GuildNews
+      autoload :Base, 'guild_news/base'
+    end
+  end
+end
---
+++ lib/wowr/armory/guild_news/base.rb
@@ -0,0 +1,11 @@
+module Wowr
+  module Armory
+    module GuildNews
+      class Base
+
+        def initialize(elem)
+        end
+      end
+    end
+  end
+end
